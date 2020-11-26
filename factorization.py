411f707c9adba23253b5c837e462a3ab43a10af4
Tianyi Liu <mrbaoziii@gmail.com>
2020-11-25 21:19:54 -0500

    implement KL divergence

--- factorization.py
+++ factorization.py
@@ -13,8 +13,35 @@
 import numpy as np
 
 
-class iNMF():
+class MovingAverage:
+    def __init__(self, k, condition=1e-5):
+        self.k = k
+        self.condition = condition
+        self.container = [np.inf * k]
+        self.ma = np.inf
+        self.ma_prev = np.inf
+
+    def update_ma(self, obj_val):
+        self.container.insert(0, obj_val)
+        self.container.pop()
+        self._cal_ma()
+
+    def _cal_ma(self):
+        if np.inf not in self.container:
+            self.ma_prev = self.ma
+            self.ma = sum(self.container) / len(self.container)
+
+    def is_converge(self):
+        if self.ma > self.ma_prev or (np.inf in self.container) or self.ma_prev == np.inf:
+            return False
+        else:
+            return ((self.ma_prev - self.ma) / self.ma_prev) < self.condition
+
+
+class iNMF:
     def __init__(self, x1, x2, k, lam, gam, penalty=True, metric="Frobenius"):
+        assert x1.shape[0] == x2.shape[0] if penalty is False else x1.shape == x2.shape
+        assert metric in ["Frobenius", "kld"]
         self.x1 = x1
         self.x2 = x2
         self.k = k
@@ -22,9 +49,6 @@ class iNMF():
         self.gam = gam
         self.metric = metric
         self.penalty = penalty
-
-        assert x1.shape[0] == x2.shape[0]
-
         self.mat_w = np.abs(np.random.randn(x1.shape[0], k)) + 0.1
         self.mat_v1 = np.abs(np.random.randn(x1.shape[0], k)) + 0.1
         self.mat_v2 = np.abs(np.random.randn(x2.shape[0], k)) + 0.1
@@ -62,6 +86,7 @@ class iNMF():
 
     def cal_gradient(self):
         if self.metric == "Frobenius":
+
             _x1h1t = np.dot(self.x1, self.mat_h1.T)
             _x2h2t = np.dot(self.x2, self.mat_h2.T)
             _wv1h1h1t = np.linalg.multi_dot([self.mat_w + self.mat_v1, self.mat_h1, self.mat_h1.T])
@@ -94,41 +119,96 @@ class iNMF():
             raise NotImplementedError
 
     def cal_grad_w(self):
-        _x1h1t = np.dot(self.x1, self.mat_h1.T)
-        _x2h2t = np.dot(self.x2, self.mat_h2.T)
-        _wv1h1h1t = np.linalg.multi_dot([self.mat_w + self.mat_v1, self.mat_h1, self.mat_h1.T])
-        _wv2h2h2t = np.linalg.multi_dot([self.mat_w + self.mat_v2, self.mat_h2, self.mat_h2.T])
-        grad_w = np.divide(_x1h1t + _x2h2t, _wv1h1h1t + _wv2h2h2t)
-        return grad_w
+        if self.metric == "Frobenius":
+            grad_w = np.divide(self._cal_term("x1h1t") + self._cal_term("x2h2t"),
+                               self._cal_term("wv1h1h1t") + self._cal_term("wv2h2h2t"))
+            return grad_w
+        elif self.metric == "kld":
+            _numerator = np.dot(self._cal_term("x1dwv1h1", self._cal_term("wv1h1")), self.mat_h1.T) \
+                         + np.dot(self._cal_term("x2dwv2h2", self._cal_term("wv2h2")), self.mat_h2.T)
+            _denominator = np.array([np.sum(self.mat_h1, axis=1) for _ in range(self.mat_w.shape[0])]) \
+                           + np.array([np.sum(self.mat_h2, axis=1) for _ in range(self.mat_w.shape[0])])
+            return np.divide(_numerator, _denominator)
 
     def cal_grad_v(self):
-        _x1h1t = np.dot(self.x1, self.mat_h1.T)
-        _x2h2t = np.dot(self.x2, self.mat_h2.T)
-        _wv1h1h1t = np.linalg.multi_dot([self.mat_w + self.mat_v1, self.mat_h1, self.mat_h1.T])
-        _wv2h2h2t = np.linalg.multi_dot([self.mat_w + self.mat_v2, self.mat_h2, self.mat_h2.T])
-        _v1h1h1t = np.linalg.multi_dot([self.mat_v1, self.mat_h1, self.mat_h1.T])
-        _v2h2h2t = np.linalg.multi_dot([self.mat_v2, self.mat_h2, self.mat_h2.T])
-
-        grad_v1 = np.divide(2 * _x1h1t, 2 * _wv1h1h1t + self.lam * _v1h1h1t)
-        grad_v2 = np.divide(2 * _x2h2t, 2 * _wv2h2h2t + self.lam * _v2h2h2t)
-        return [grad_v1, grad_v2]
+        if self.metric == "Frobenius":
+            grad_v1 = np.divide(self._cal_term("x1h1t"),
+                                self._cal_term("wv1h1h1t") + self.lam * self._cal_term("v1h1h1t"))
+            grad_v2 = np.divide(self._cal_term("x2h2t"),
+                                self._cal_term("wv2h2h2t") + self.lam * self._cal_term("v2h2h2t"))
+            return [grad_v1, grad_v2]
+        elif self.metric == "kld":
+            _numerator1 = np.dot(self._cal_term("x1dwv1h1", self._cal_term("wv1h1")), self.mat_h1.T)
+            _numerator2 = np.dot(self._cal_term("x2dwv2h2", self._cal_term("wv2h2")), self.mat_h1.T)
+            _denominator1 = np.array([np.sum(self.mat_h1, axis=1) for _ in range(self.mat_w.shape[0])]) \
+                            + 2 * self.lam * self._cal_term("v1h1h1t")
+            _denominator2 = np.array([np.sum(self.mat_h2, axis=1) for _ in range(self.mat_w.shape[0])]) \
+                            + 2 * self.lam * self._cal_term("v2h2h2t")
+            return [np.divide(_numerator1, _denominator1), np.divide(_numerator2, _denominator2)]
 
     def cal_grad_h(self):
-        _wv1tx1 = np.dot(self.mat_w.T + self.mat_v1.T, self.x1)
-        _wv2tx2 = np.dot(self.mat_w.T + self.mat_v2.T, self.x2)
-        _wv1twv1h1 = np.linalg.multi_dot([self.mat_w.T + self.mat_v1.T, self.mat_w + self.mat_v1, self.mat_h1])
-        _wv2twv2h2 = np.linalg.multi_dot([self.mat_w.T + self.mat_v2.T, self.mat_w + self.mat_v2, self.mat_h2])
-        _v1tv1h1 = np.linalg.multi_dot([self.mat_v1.T, self.mat_v1, self.mat_h1])
-        _v2tv2h2 = np.linalg.multi_dot([self.mat_v2.T, self.mat_v2, self.mat_h2])
-        if self.penalty is False:
-            grad_h1 = np.divide(_wv1tx1, _wv1twv1h1 + self.lam * _v1tv1h1)
-            grad_h2 = np.divide(_wv2tx2, _wv2twv2h2 + self.lam * _v2tv2h2)
+        if self.metric == "Frobenius":
+            _numerator1 = self._cal_term("wv1tx1")
+            _numerator2 = self._cal_term("wv2tx2")
+            _denominator1 = self._cal_term("wv1twv1h1") + self.lam * self._cal_term("v1tv1h1")
+            _denominator2 = self._cal_term("wv2twv2h2") + self.lam * self._cal_term("v2tv2h2")
+            return [np.divide(_numerator1, _denominator1), np.divide(_numerator2, _denominator2)] \
+                if not self.penalty else \
+                [np.divide(_numerator1, _denominator1 + self.gam * (self.mat_h1 - self.mat_h2)),
+                 np.divide(_numerator2, _denominator2 + self.gam * (self.mat_h1 - self.mat_h2))]
+        elif self.metric == "kld":
+            _numerator1 = np.dot((self.mat_w + self.mat_v1).T, self._cal_term("x1dwv1h1", self._cal_term("wv1h1")))
+            _numerator2 = np.dot((self.mat_w + self.mat_v2).T, self._cal_term("x2dwv2h2", self._cal_term("wv2h2")))
+            _denominator1 = np.array([np.sum(self.mat_w + self.mat_v1, axis=0) for _ in
+                                      range(self.mat_h1.shape[1])]).T + 2 * self.lam * self._cal_term("v1tv1h1")
+            _denominator2 = np.array([np.sum(self.mat_w + self.mat_v2, axis=0) for _ in
+                                      range(self.mat_h2.shape[1])]).T + 2 * self.lam * self._cal_term("v2tv2h2")
+            return [np.divide(_numerator1, _denominator1), np.divide(_numerator2, _denominator2)] \
+                if not self.penalty else \
+                [np.divide(_numerator1, _denominator1 + self.gam * np.log(np.divide(self.mat_h1, self.mat_h2))),
+                 np.divide(_numerator2, _denominator2 + self.gam * (1 - np.divide(self.mat_h1, self.mat_h2)))]
+
+    def _cal_term(self, term, *argv):
+        if term == "x1h1t":
+            return np.dot(self.x1, self.mat_h1.T)
+        elif term == "x2h2t":
+            return np.dot(self.x2, self.mat_h2.T)
+        elif term == "wv1h1h1t":
+            return np.linalg.multi_dot([self.mat_w + self.mat_v1, self.mat_h1, self.mat_h1.T])
+        elif term == "wv2h2h2t":
+            return np.linalg.multi_dot([self.mat_w + self.mat_v2, self.mat_h2, self.mat_h2.T])
+        elif term == "wv1h1":
+            return np.dot(self.mat_w + self.mat_v1, self.mat_h1)
+        elif term == "wv2h2":
+            return np.dot(self.mat_w + self.mat_v2, self.mat_h2)
+        elif term == "x1dwv1h1":
+            return np.divide(self.x1, argv[0])
+        elif term == "x2dwv2h2":
+            return np.divide(self.x2, argv[0])
+        elif term == "v1h1h1t":
+            return np.linalg.multi_dot([self.mat_v1, self.mat_h1, self.mat_h1.T])
+        elif term == "v2h2h2t":
+            return np.linalg.multi_dot([self.mat_v2, self.mat_h2, self.mat_h2.T])
+        elif term == "wv1":
+            return
+        elif term == "v1tv1h1":
+            return np.linalg.multi_dot([self.mat_v1.T, self.mat_v1, self.mat_h1])
+        elif term == "v2tv2h2":
+            return np.linalg.multi_dot([self.mat_v2.T, self.mat_v2, self.mat_h2])
+        elif term == "wv1tx1":
+            return np.dot(self.mat_w.T + self.mat_v1.T, self.x1)
+        elif term == "wv2tx2":
+            return np.dot(self.mat_w.T + self.mat_v2.T, self.x2)
+        elif term == "wv1twv1h1":
+            return np.linalg.multi_dot([self.mat_w.T + self.mat_v1.T, self.mat_w + self.mat_v1, self.mat_h1])
+        elif term == "wv2twv2h2":
+            return np.linalg.multi_dot([self.mat_w.T + self.mat_v2.T, self.mat_w + self.mat_v2, self.mat_h2])
+        elif term == "v1tv1h1":
+            return np.linalg.multi_dot([self.mat_v1.T, self.mat_v1, self.mat_h1])
+        elif term == "v2tv2h2":
+            return np.linalg.multi_dot([self.mat_v2.T, self.mat_v2, self.mat_h2])
         else:
-            grad_h1 = 2 * np.divide(_wv1tx1, 2 * _wv1twv1h1 + 2 * self.lam * _v1tv1h1 + 2 * self.gam * (
-                        self.mat_h1 - self.mat_h2))
-            grad_h2 = 2 * np.divide(_wv2tx2, 2 * _wv2twv2h2 + 2 * self.lam * _v2tv2h2 + 2 * self.gam * (
-                        self.mat_h2 - self.mat_h1))
-        return [grad_h1, grad_h2]
+            raise NameError("Invalid Term: {}".format(term))
 
     def update_par(self):
         # Use latest pars for gradient
@@ -144,6 +224,5 @@ class iNMF():
         self.mat_h2 *= grad_h[1]
 
     def current_par(self):
-        return [self.mat_w, self.mat_v1, self.mat_v2, self.mat_h1, self.mat_h2]
-
+        return {"w": self.mat_w, "v1": self.mat_v1, "v2": self.mat_v2, "h1": self.mat_h1, "h2": self.mat_h2}
 
